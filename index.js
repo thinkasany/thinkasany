180d285f9730e27d4cf2d55fbf1b2c48e85d73f6
thinkasany <480968828@qq.com>
2024-06-18 15:34:38 +0800

    update

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ const imglist = [
 ];
 
 /** 接口有时候数据会炸 API rate limit exceeded，做一份数据备份， 需要使用node18才能正常运行 */
-const numsArr = [67, 125, 7, 27, 185, 3, 4, 24, 64];
+const numsArr = [67, 130, 7, 27, 186, 3, 4, 24, 74];
 const tmp = [];
 console.log("histroy numsArr", numsArr);
 const returnSvgList = async () => {
