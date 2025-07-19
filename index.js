650afdb8dc9fe201ee2d47c452d6c614a4d49352
thinkasany <480968828@qq.com>
2025-07-19 12:53:00 +0800

    update

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const imglist = [
 // https://github.com/element-plus/element-plus/graphs/contributors
 
 /** 接口有时候数据会炸 API rate limit exceeded，做一份数据备份， 需要使用node18才能正常运行 */
-const numsArr = [221, 196, 14, 7, 27, 186, 3, 4, 24, 207];
+const numsArr = [279, 196, 14, 7, 27, 186, 3, 4, 24, 207];
 const tmp = [];
 console.log("histroy numsArr", numsArr);
 const returnSvgList = async () => {
